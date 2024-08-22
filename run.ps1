9b3060f4d3f26d7e0109f8d67976a71b5e528116
justt1n <tinnt0709@gmail.com>
2024-08-22 18:16:08 +0700

    update

--- run.ps1
+++ run.ps1
@@ -1,2 +1,9 @@
-/myenv/bin/activate
+echo "Running Script"
+echo "Checking for update"
+git pull
+echo "Update completed"
+& myenv\Scripts\Activate.ps1
+echo "Running Script..."
 python script.py
+echo "Complete"
+Read-Host -Prompt "Press Enter to exit"
