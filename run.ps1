fd4478fba23528e5c7bb9b84f08cdcffe78acc49
justt1n <tinnt0709@gmail.com>
2024-08-25 15:20:20 +0700

    update

--- run.ps1
+++ run.ps1
@@ -1,9 +1,9 @@
 echo "Running Script"
 echo "Checking for update"
+git reset --hard origin/master
 git pull
 echo "Update completed"
 & myenv\Scripts\Activate.ps1
 echo "Running Script..."
 python script.py
 echo "Complete"
-Read-Host -Prompt "Press Enter to exit"
